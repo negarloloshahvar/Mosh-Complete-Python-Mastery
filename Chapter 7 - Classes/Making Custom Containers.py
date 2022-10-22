dbed0c384e64de8c705b5cf3205b5d5c481d27da
Negar Loloshahvar <negarshahvar@gmail.com>
2022-10-22 14:05:00 +0100

    Update Making Custom Containers.py

--- Chapter 7 - Classes/Making Custom Containers.py
+++ Chapter 7 - Classes/Making Custom Containers.py
@@ -1,6 +1,37 @@
 class BlogTags:
     def __init__(self):
-        self.tags = {}
+        self.__tags = {}
+
+    def show(self):
+        print(self.__tags)
 
     def add(self, tag):
-       self.tags[tag] = self.tags.get(tag, 0) + 1
+       self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
+
+    def __getitem__(self, tag):
+        return self.__tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.__tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.__tags)
+
+    def __iter__(self):
+        return iter(self.__tags)
+
+cloud = BlogTags()
+
+cloud.add('Python')
+cloud.add('python')
+cloud.add('python')
+cloud.add('python')
+cloud.add('java')
+cloud.add('java')
+cloud.add('css')
+cloud.add('javascript')
+cloud['html'] = 10
+
+cloud.show()
+print(cloud['PYTHON'])
+
